7cacac63c17e4664e9b335b3a30ef067ca07976f
orangegrove_1955 <orangegrove_1955@hotmail.com>
2019-09-18 11:21:52 +1000

    Trying out a test branch to see deploy previews in netlify

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,8 +9,7 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <p>This index is located on test-branch</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
